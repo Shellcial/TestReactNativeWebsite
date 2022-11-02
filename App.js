0c689e2294361e1f9a141087369720013e468bb4
myusername <turtlecatpurr@gmail.com>
2022-11-02 21:32:25 +0800

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { SketchPicker } from 'react-color';
+import { ChromePicker } from 'react-color';
+import { useState } from 'react';
 export default function App() {
+  let isMeow = true
+  const [newColor, setColor] = useState('#FFF')
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <ChromePicker color={newColor} onChange={(updatedColor) => { setColor(updatedColor.hex) }} />
       <StatusBar style="auto" />
     </View>
   );
